e6e23ce7a8235d4e9635fb41c282b3bfe59fe2e0
Irving Frausto <saulfrausto0@gmail.com>
2025-06-09 14:25:02 -0700

    User Zodiac Sign
    User inputs information to figure out Zodiac Sign

---
+++ p14.py
@@ -0,0 +1,45 @@
+#Program Name: p14.py
+#My Name: Irving Frausto 
+#Date Started - Date Completed: 02/27/2025
+# Python 3.12.4
+# Description: ZODIAC SIGNS :0
+
+day = int(input('please enter day:'))
+month = int(input('please enter month:'))
+if (month == 3 and day >= 21 ) or  (month == 4 and day <= 19):
+    print("You are Aries")
+if (month == 4 and day >= 20 ) or  (month == 5 and day <= 20):
+    print("You are Taurus")
+if (month == 5 and day >= 21 ) or  (month == 6 and day <= 21):
+    print("You are Gemini")
+if (month == 6 and day >= 22 ) or  (month == 7 and day <= 22):
+    print("You are Cancer")
+if (month == 7 and day >= 23 ) or  (month == 8 and day <= 22):
+    print("You are Leo")
+if (month == 8 and day >= 23 ) or  (month == 9 and day <= 22):
+    print("You are Virgo")
+if (month == 9 and day >= 23 ) or  (month == 10 and day <= 23):
+    print("You are Libra")
+if(month == 10 and day >= 24 ) or (month == 11 and day <= 21):
+    print("You are Scorpio")
+if (month == 11 and day >= 22) or (month == 12 and day <= 21):
+    print("You are Sagittarius")
+if(month == 12 and day >= 22) or (month == 1 and day <= 19):
+    print("You are Capricorn")
+if(month == 1 and day >= 20) or (month == 2 and day <= 18):
+    print("You are Aquarius")
+if(month == 2 and day >= 19) or (month == 3 and day <= 20):
+    print("You are Pisces")
+'''
+
+= RESTART: /Users/saulfrausto/Desktop/De Anza College /Intro to Coding - Python/Week 2/Week 2A/p14.py
+please enter day 30
+please enter month 8
+You are Virgo
+
+= RESTART: /Users/saulfrausto/Desktop/De Anza College /Intro to Coding - Python/Week 2/Week 2A/p14.py
+please enter day:30
+please enter month:9
+You are Libra
+
+'''
